1d1889cabf4d8f4c22b56714c94fa5b19fb62adf
Harsh Pandhe <pandhemihir@gmail.com>
2024-08-03 21:30:33 +0530

    Update 03

--- app.c
+++ app.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+
 #define SIZE 3
+
 
 char board[SIZE][SIZE];
 char currentPlayer = 'X';
+
 
 void initializeBoard() {
     for (int i = 0; i < SIZE; i++) {
@@ -13,6 +16,7 @@ void initializeBoard() {
         }
     }
 }
+
 
 void displayBoard() {
     printf("\n");
@@ -27,19 +31,22 @@ void displayBoard() {
     printf("\n");
 }
 
+
 void switchPlayer() {
     currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
 }
+
 
 bool makeMove(int row, int col) {
     if (row >= 0 && row < SIZE && col >= 0 && col < SIZE && board[row][col] == ' ') {
         board[row][col] = currentPlayer;
         return true;
     } else {
-        printf("Invalid move. Try again.\n");
+        printf("Invalid move. Please enter a valid move.\n");
         return false;
     }
 }
+
 
 bool checkWin() {
     for (int i = 0; i < SIZE; i++) {
@@ -49,8 +56,10 @@ bool checkWin() {
     if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) return true;
     if (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer) return true;
 
+
     return false;
 }
+
 
 bool checkDraw() {
     for (int i = 0; i < SIZE; i++) {
@@ -61,16 +70,36 @@ bool checkDraw() {
     return true;
 }
 
+
+void getPlayerMove(int *row, int *col) {
+    while (true) {
+        printf("Player %c, enter your move (row and column): ", currentPlayer);
+        scanf("%d %d", row, col);
+        if (*row >= 1 && *row <= SIZE && *col >= 1 && *col <= SIZE) {
+            (*row)--;
+            (*col)--;
+            if (board[*row][*col] == ' ') {
+                break;
+            } else {
+                printf("This cell is already occupied. Please enter a different move.\n");
+            }
+        } else {
+            printf("Invalid input. Please enter row and column numbers between 1 and 3.\n");
+        }
+    }
+}
+
+
 int main() {
     int row, col;
     bool gameOver = false;
     initializeBoard();
 
+
     while (!gameOver) {
         displayBoard();
-        printf("Player %c, enter your move (row and column): ", currentPlayer);
-        scanf("%d %d", &row, &col);
-        if (makeMove(row - 1, col - 1)) {
+        getPlayerMove(&row, &col);
+        if (makeMove(row, col)) {
             if (checkWin()) {
                 displayBoard();
                 printf("Player %c wins!\n", currentPlayer);
@@ -84,6 +113,7 @@ int main() {
             }
         }
     }
+
 
     return 0;
 }
