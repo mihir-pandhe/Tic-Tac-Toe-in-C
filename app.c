b07fca37dcf06ed4ae7b87316066d2b0d41fd2bc
Harsh Pandhe <pandhemihir@gmail.com>
2024-08-03 21:04:54 +0530

    Update 02

--- app.c
+++ app.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define SIZE 3
 
@@ -30,25 +31,58 @@ void switchPlayer() {
     currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
 }
 
-void makeMove(int row, int col) {
+bool makeMove(int row, int col) {
     if (row >= 0 && row < SIZE && col >= 0 && col < SIZE && board[row][col] == ' ') {
         board[row][col] = currentPlayer;
+        return true;
     } else {
         printf("Invalid move. Try again.\n");
-        switchPlayer();
+        return false;
     }
+}
+
+bool checkWin() {
+    for (int i = 0; i < SIZE; i++) {
+        if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) return true;
+        if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) return true;
+    }
+    if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) return true;
+    if (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer) return true;
+
+    return false;
+}
+
+bool checkDraw() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (board[i][j] == ' ') return false;
+        }
+    }
+    return true;
 }
 
 int main() {
     int row, col;
+    bool gameOver = false;
     initializeBoard();
 
-    while (1) {
+    while (!gameOver) {
         displayBoard();
         printf("Player %c, enter your move (row and column): ", currentPlayer);
         scanf("%d %d", &row, &col);
-        makeMove(row - 1, col - 1);
-        switchPlayer();
+        if (makeMove(row - 1, col - 1)) {
+            if (checkWin()) {
+                displayBoard();
+                printf("Player %c wins!\n", currentPlayer);
+                gameOver = true;
+            } else if (checkDraw()) {
+                displayBoard();
+                printf("The game is a draw!\n");
+                gameOver = true;
+            } else {
+                switchPlayer();
+            }
+        }
     }
 
     return 0;
